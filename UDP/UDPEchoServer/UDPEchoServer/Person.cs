b47b4c9b9da1848d0ff49ffa1949101321ad52f1
mart834d <36763176+mart834d@users.noreply.github.com>
2019-05-09 09:23:53 +0200

    Har lavert Database connection, samt vores UDP Reciever færdig.

---
+++ UDP/UDPEchoServer/UDPEchoServer/Person.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UDPEchoServer
+{
+    public static class Person
+    {
+        private static SqlConnection connection = new SqlConnection();
+        public static SqlConnection GetConnection()
+        {
+            if (connection.State == ConnectionState.Closed)
+            {
+                
+                connection.ConnectionString = @"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=master;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+                connection.Open();
+            }
+            return connection;
+        }
+
+        public static bool createPerson(PersonDTO model)
+        {
+            try
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    cmd.CommandText = $@"insert into Person (Fornavn, Efternavn, Email) VALUES ('{model.Fornavn}', '{model.Efternavn}', '{model.Email}');SCOPE_IDENTITY()";
+                    model.Id = Convert.ToInt32(cmd.ExecuteScalar());
+                    model.Data.FK_Person = model.Id;
+                }
+
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+
+            return setPersonData(model.Data);
+        }
+        public static bool setPersonData(PersonDataDTO model)
+        {
+            try
+            {
+                using (var cmd = connection.CreateCommand())
+                {
+                    //UPDATE table_name
+                    //SET column1 = value1, column2 = value2, ...
+                    //WHERE id = 0;
+                    cmd.CommandText = $@"insert into PersonData (FK_Person, Hastighed, Acceleration) VALUES ({model.FK_Person}, {model.Hastighed}, {model.Acceleration}')";
+                    cmd.ExecuteNonQuery();
+                }
+                return true;
+            }
+            catch (Exception e)
+            {
+                return false;
+            }
+        }
+
+        public class PersonDTO
+        {
+            
+            public int Id { get; set; }
+            public string Fornavn { get; set; }
+            public string Efternavn { get; set; }
+            public string Email { get; set; }
+            public PersonDataDTO Data { get; set; } = new PersonDataDTO();
+        
+
+        }
+        public class PersonDataDTO
+        {
+            public int Id { get; set; }
+            public int FK_Person { get; set; }
+            public int Hastighed { get; set; }
+            public double Acceleration { get; set; }
+        }
+    }
+}
---
+++ UDP/UDPEchoServer/UDPEchoServer/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace UDPEchoServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person.GetConnection();
+
+            //Creates a UdpClient for reading incoming data.
+            UdpClient udpServer = new UdpClient(1111);
+
+            //Creates an IPEndPoint to record the IP Address and port number of the sender.  
+            IPAddress ip = IPAddress.Parse("192.168.1.33");
+            IPEndPoint RemoteIpEndPoint = new IPEndPoint(ip, 1111);
+           
+            try
+            {
+                // Blocks until a message is received on this socket from a remote host (a client).
+                Console.WriteLine("Server is blocked");
+                var list = new List<double>();
+                var listTid = new List<DateTime>();
+                while (true)
+                {
+                    
+                    Byte[] receiveBytes = udpServer.Receive(ref RemoteIpEndPoint);
+                    //Server is now activated");
+
+                    string receivedData = Encoding.ASCII.GetString(receiveBytes);
+                    string[] data = receivedData.Split(' ');
+
+                    double force = 0;
+                    if (!string.IsNullOrEmpty(data[8]))
+                        force = Convert.ToDouble(data[8]);
+
+                    DateTime tid = new DateTime();
+                    if(!string.IsNullOrEmpty(data[3]))
+                        tid = Convert.ToDateTime(data[3]);
+
+                    string text = "Tid: " + tid.ToString() + " Force: " + force;
+                    Console.WriteLine(text);
+
+                    list.Add(force);
+                    listTid.Add(tid);
+
+                    if (receivedData.Contains("stop"))
+                        break;
+
+                    string sendData = "Server: " + text.ToUpper();
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(sendData);
+
+                    udpServer.Send(sendBytes, sendBytes.Length, RemoteIpEndPoint);
+
+                   
+                }
+                var avg = list.Average();
+                var lastTime = listTid.Last();
+                var firstTime = listTid.First();
+                var time = lastTime - firstTime;
+                
+
+                Person.PersonDTO person = new Person.PersonDTO();
+                person.Fornavn = "Frank";
+                person.Data.Acceleration = avg;
+                person.Data.Hastighed = 0;
+
+                var success = Person.createPerson(person);
+
+                Console.WriteLine($"Average: {avg} - Time: {lastTime}");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+        }
+        
+    }
+}
